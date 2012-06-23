ea52896defd2734209b19947bbaec0a357b7d880
DeanThomas1983 <dean.plymouth@googlemail.com>
2012-06-23 15:19:24 +0100

    Create Java project

---
+++ CyrillicLanguageHelper/src/cyrilliclanguagehelper/CyrillicLanguageHelper.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cyrilliclanguagehelper;
+
+/**
+ * Cyrillic Language Helper
+ * @author Dean Thomas
+ * @version 0.1
+ * 
+ */
+public class CyrillicLanguageHelper {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
