295e21ac98d04f8995d63ffb316d64b9c148b780
DeanThomas1983 <dean.plymouth@googlemail.com>
2012-06-24 15:11:41 +0100

    Pass the "File" object to the Language constructor rather than the Filename string.

--- CyrillicLanguageHelper/src/cyrilliclanguagehelper/Language.java
+++ CyrillicLanguageHelper/src/cyrilliclanguagehelper/Language.java
@@ -4,14 +4,19 @@
  */
 package cyrilliclanguagehelper;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.xml.sax.*;
 
 /**
  * @class Language
  * @author Dean Thomas
  */
 public class Language extends ArrayList {
-    private String title;
+    private File fileName;
     
     /**
      * Default constructor
@@ -23,19 +28,103 @@ public class Language extends ArrayList {
         System.out.println("New language object created");
     }
     
-    /**
-     * Load from XML
-     * 
-     * Load a language definition from an XML document
+    /** Load a language definition from an XML document
      * 
      * @param fileName the XML document containing the definitions
      */
-    public Language(String fileName)
+    public Language(File fileName)
     {
         this();
      
-        this.title = fileName;
+        this.fileName = fileName;
         
-        System.out.println(title);
+        System.out.println(this.fileName.getName());
+        
+        parseXMLFile();
+    }
+    
+    //  TODO: move this to a separate class?
+    private void parseXMLFile()
+    {
+        XMLReader xmlReader = new XMLReader() {
+
+            @Override
+            public boolean getFeature(String name) throws SAXNotRecognizedException, SAXNotSupportedException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void setFeature(String name, boolean value) throws SAXNotRecognizedException, SAXNotSupportedException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public Object getProperty(String name) throws SAXNotRecognizedException, SAXNotSupportedException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void setProperty(String name, Object value) throws SAXNotRecognizedException, SAXNotSupportedException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void setEntityResolver(EntityResolver resolver) {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public EntityResolver getEntityResolver() {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void setDTDHandler(DTDHandler handler) {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public DTDHandler getDTDHandler() {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void setContentHandler(ContentHandler handler) {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public ContentHandler getContentHandler() {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void setErrorHandler(ErrorHandler handler) {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public ErrorHandler getErrorHandler() {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void parse(InputSource input) throws IOException, SAXException {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void parse(String systemId) throws IOException, SAXException {
+                System.out.println("Begin parsing " + fileName);
+                //throw new UnsupportedOperationException("Not supported yet.");
+            }
+        };
+        try {
+            xmlReader.parse(this.fileName.toString());
+        } catch (IOException ex) {
+            Logger.getLogger(Language.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(Language.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
